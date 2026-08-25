e2b8035a60734a31ee186d9dfb8a3ca8e7574f15
Leart Dukaj <leartdukaj@tutanota.de>
2026-08-26 00:50:50 +0200

    made install script compatible with arch linux and script now uses a virtual environment

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
-sudo apt-get update -y
-sudo apt install python3-brotli -y
-sudo apt install yt-dlp -y
-sudo apt install ffmpeg -y
-pip3 install pillow
-pip3 install numpy
-pip3 install playsound
-pip3 install tqdm
-pip3 install pygame
+#!/bin/bash
+source /etc/os-release
+
+case "$ID" in
+  debian|ubuntu)
+    sudo apt-get update
+    sudo apt-get install -y python3-brotli yt-dlp ffmpeg
+    ;;
+  arch)
+    sudo pacman -Syu --noconfirm python-brotli yt-dlp ffmpeg
+    ;;
+esac
+
+python3 -m venv .
+source bin/activate
+pip install -r requirements.txt
 chmod +x remote.sh
 echo 'All done! To run the code, type python3 launcher.py'
